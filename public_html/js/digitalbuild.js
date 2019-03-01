954fddd96b1d095de10dcc4c9f360483db818b59
digitalbuilonline@gmail.com <digitalbuildonline@gmail.com>
2019-03-01 18:07:44 -0300

    se actualiza la rama con los cambios en QA

--- public_html/js/digitalbuild.js
+++ public_html/js/digitalbuild.js
@@ -6,9 +6,40 @@
 
 
 $(document).ready(function () {
-    helloWord();
-    }
+  $('#error').html("Estamos actualizando la geolocalizacion...");
+  helloWordGps();
+  helloWord();
+    
+}
 );
+
+function helloWordGps () {
+    var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+	$('#error').html('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+    
+    function onError(error) {
+        alert('Error al buscar la geolocalizacion: code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+ 
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
 
 
 function helloWord() {
